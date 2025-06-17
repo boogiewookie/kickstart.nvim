27ea0714d1fec0f4c38b1b10295095428233ac27
Kevet Duncombe <kevetduncombe@gmail.com>
2025-06-17 15:44:52 -0500

    add GhostText plugin

--- init.lua
+++ init.lua
@@ -260,6 +260,7 @@ rtp:prepend(lazypath)
 require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'NMAC427/guess-indent.nvim', -- Detect tabstop and shiftwidth automatically
+  'subnut/nvim-ghost.nvim', -- Act as a server for GhostText
 
   -- NOTE: Plugins can also be added by using a table,
   -- with the first argument being the link and the following
