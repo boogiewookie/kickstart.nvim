33ee22f2ed7393be15312648c1b56ffaab50edd6
Kevet Duncombe <kevetduncombe@gmail.com>
2025-12-18 17:39:45 -0600

    switch to kanagawa color scheme

--- init.lua
+++ init.lua
@@ -890,25 +890,31 @@ require('lazy').setup({
     },
   },
 
-  { -- You can easily change to a different colorscheme.
-    -- Change the name of the colorscheme plugin below, and then
-    -- change the command in the config to whatever the name of that colorscheme is.
-    --
-    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'folke/tokyonight.nvim',
-    priority = 1000, -- Make sure to load this before all the other start plugins.
+  {
+    'rebelot/kanagawa.nvim',
+    lazy = false, -- Load immediately
+    priority = 1000, -- Ensure it loads before other UI plugins
     config = function()
-      ---@diagnostic disable-next-line: missing-fields
-      require('tokyonight').setup {
-        styles = {
-          comments = { italic = false }, -- Disable italics in comments
-        },
+      require('kanagawa').setup {
+        commentStyle = { italic = true },
+        keywordStyle = { italic = true },
+        -- This 'overrides' section is the magic fix for contrast
+        overrides = function(colors)
+          local theme = colors.theme
+          return {
+            -- This makes comments significantly easier to read
+            -- 'fujiGray' is the standard, but you can use 'oldWhite' for max contrast
+            Comment = { fg = colors.palette.fujiGray, italic = true },
+
+            -- Optional: Makes the floating windows (like LSPs) look cleaner
+            NormalFloat = { bg = 'none' },
+            FloatBorder = { bg = 'none' },
+          }
+        end,
       }
 
-      -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
+      -- Final command to set the theme
+      vim.cmd 'colorscheme kanagawa'
     end,
   },
 
@@ -1025,6 +1031,11 @@ require('lazy').setup({
     },
   },
 })
+
+-- Create a user command :GeminiLinksToTwiki
+vim.api.nvim_create_user_command('GeminiLinksToTwiki', function()
+  vim.cmd 'g;\\[\\[\\([^]]*\\)\\](\\([^)]*\\))\\];s;;[[\\2][\\1]];g'
+end, {})
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
