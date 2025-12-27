70e7b994e7da297f3ba98d98903fd97f9935361b
Kevet Duncombe <kevetduncombe@gmail.com>
2025-12-27 11:30:55 -0600

    tweak stylua config

--- init.lua
+++ init.lua
@@ -788,6 +788,12 @@ require('lazy').setup({
         -- You can use 'stop_after_first' to run the first available formatter from the list
         -- javascript = { "prettierd", "prettier", stop_after_first = true },
       },
+      formatters = {
+        stylua = {
+          -- Overriding the command arguments to remove any potential --lsp flags
+          args = { '--search-parent-directories', '--stdin-filepath', '$FILENAME', '-' },
+        },
+      },
     },
   },
 
